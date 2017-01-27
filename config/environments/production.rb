8f583374e3a366e046a9f7c22971a1710a261c30
mayu <mayutaro89@yahoo.co.jp>
2017-01-27 02:48:20 +0000

    mailform

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "lesson_#{Rails.env}"
   config.action_mailer.perform_caching = false
 config.action_mailer.default_url_options = { :host => 'your_app.herokuapp.com' }
+config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    #:enable_starttls_auto => false,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'localhost:3000',
+    :authentication => :plain,
+    :user_name => 'dmpp1204@gmail.com',
+    :password => 'myjineru420',
+    :enable_starttls_auto=> true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
